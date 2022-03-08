e131d5776ef55ecee2b70cf380a0689aa88d0518
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-08 20:46:37 +0200

    add gulp size file

--- tasks/css.js
+++ tasks/css.js
@@ -2,13 +2,16 @@ const { src, dest } = require("gulp");
 
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
-var csso = require("gulp-csso");
+const csso = require("gulp-csso");
+const size = require('gulp-size');
 
 const css = () => {
   return src("./src/css/style.css")
-    .pipe(autoprefixer({}))
-    .pipe(csso())
     .pipe(concat("style.min.css"))
+    .pipe(autoprefixer({}))
+    .pipe(size({title: "css before compress"}))
+    .pipe(csso())
+    .pipe(size({title: "css after compress"}))
     .pipe(dest("./dist/css/"));
   //  .pipe(browserSync.stream());
 };
--- tasks/html.js
+++ tasks/html.js
@@ -1,10 +1,14 @@
 const { src, dest } = require("gulp");
 
 const htmlmin = require("gulp-htmlmin");
+const size = require('gulp-size');
+
 
 const html = () => {
   return src("./src/index.html")
+    .pipe(size({title: "html before compress"}))
     .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(size({title: "html after compress"}))
     .pipe(dest("./dist"));
   //  .pipe(browserSync.stream());
 };
