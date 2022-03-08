3a81552d8f211ec4c45e1490483679518f847330
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-08 20:39:50 +0200

    add plugin csso

--- tasks/css.js
+++ tasks/css.js
@@ -1,11 +1,13 @@
 const { src, dest } = require("gulp");
 
 const concat = require("gulp-concat");
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require("gulp-autoprefixer");
+var csso = require("gulp-csso");
 
 const css = () => {
   return src("./src/css/style.css")
     .pipe(autoprefixer({}))
+    .pipe(csso())
     .pipe(concat("style.min.css"))
     .pipe(dest("./dist/css/"));
   //  .pipe(browserSync.stream());
