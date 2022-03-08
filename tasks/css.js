1a4f2520d8d0f2cde1e6fd5e5efcb9d46047e443
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-08 20:36:47 +0200

    add autoprefixer

--- tasks/css.js
+++ tasks/css.js
@@ -1,9 +1,11 @@
 const { src, dest } = require("gulp");
 
 const concat = require("gulp-concat");
+const autoprefixer = require('gulp-autoprefixer');
 
 const css = () => {
   return src("./src/css/style.css")
+    .pipe(autoprefixer({}))
     .pipe(concat("style.min.css"))
     .pipe(dest("./dist/css/"));
   //  .pipe(browserSync.stream());
