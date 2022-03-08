450e55007c8faee21a4ac0f6269201f0513664ff
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-08 19:52:22 +0200

    add Browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 
+var browserSync = require('browser-sync').create();
+
 const html = require('./tasks/html');
 
+const server = () => {
+   browserSync.init({
+      server: {
+          baseDir: "./dist"
+      }
+  });
+}
+
+
 const watcher = () => {
-   watch('./src/**/*.html', html);
+   watch('./src/**/*.html', html).on('change', browserSync.reload);
 }
 
 exports.html = html;
 exports.watch = watcher;
 
-exports.dev = series (html, watcher);
+exports.dev = series (
+   html, 
+   parallel (watcher, server)
+);
--- tasks/html.js
+++ tasks/html.js
@@ -1,11 +1,12 @@
-const { src, dest } = require('gulp')
+const { src, dest } = require("gulp");
 
-const htmlmin = require('gulp-htmlmin');
+const htmlmin = require("gulp-htmlmin");
 
 const html = () => {
-   return src('./src/index.html')
-      .pipe(htmlmin({ collapseWhitespace: true }))
-      .pipe(dest('./dist'))   
-}
+  return src("./src/index.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest("./dist"))
+   //  .pipe(browserSync.stream());
+};
 
 module.exports = html;
