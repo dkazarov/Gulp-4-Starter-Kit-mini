7ed9737671b2e8c735a46295b3a2d955492e4bb0
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-08 19:41:49 +0200

    add watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
-const { src, dest } = require('gulp')
-
-// const htmlmin = require('gulp-htmlmin');
-
-// const html = () => {
-//    return src('./src/index.html')
-//       .pipe(htmlmin({ collapseWhitespace: true }))
-//       .pipe(dest('./dist'))   
-// }
+const { src, dest, watch, series, parallel } = require('gulp');
 
 const html = require('./tasks/html');
 
+const watcher = () => {
+   watch('./src/**/*.html', html);
+}
+
 exports.html = html;
+exports.watch = watcher;
+
+exports.dev = series (html, watcher);
